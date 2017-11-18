402b2e23572f8c5421688d4b20375a8ef2c8fba3
Marco Ferrari <ferrari.marco@gmail.com>
2017-11-18 11:14:50 +0100

    Removed unneeded comments

--- debugfs/recursive-ls.sh
+++ debugfs/recursive-ls.sh
@@ -1,24 +1,5 @@
 #!/bin/sh
 
-# /150880270/040770/500/100/.//
-# /150863873/040777/0/0/..//
-# /150888451/040770/500/100/temp//
-# /161612929/040770/0/0/Arrow S05E23//
-# /150888455/040770/500/100/monitored//
-# /161612927/040770/0/0/Sarah Maas - La corona di fuoco//
-# /161612931/040770/0/0/William Nicholson - Il Canto Delle Fiamme//
-# /161612935/100660/0/0/Zio Paperone 017 (Disney 1991-02) [c2c Frank54 & CapitanUltra].cbr/52088041/
-# /161612932/100660/0/0/Dante Alighieri - La divina commedia (Utet, Torino, 2013).pdf/8572224/
-# /161612947/100660/0/0/I classici di Walt Disney 043 - Paperino Bang (Mondadori 1980-07) [c2c Fosforo & Aquila & Bibbo64]FIX.cbr/129935407/
-# /161612945/100660/0/0/I classici di Walt Disney 044 - Paperin Sansone (Mondadori 1980-08) [c2c Fosforo & Aquila & Bibbo64]FIX.cbr/139668310/
-# /161612920/040770/0/0/Dylan Dog i colori della paura 31-40//
-# /161612922/100660/0/0/Dylan _ N.362 - Dopo un lungo silenzio by Lollo - Sergio Bonelli.cbr/57431944/
-# /161612928/100660/0/0/The.Blacklist.5x02.Greyson.Blaise.ITA.ENG.720p.AMZN.WEBMux.x264-Morpheus.mkv/1902729404/
-# /161612904/100660/0/0/La Torre Nera (2017) 1080p AC3 ITA DTS AC3 ENG.mkv/8908036583/
-# /161612640/100660/0/0/USS Indianapolis (2016) 1080p DTS AC3 ITA ENG.mkv/11760667669/
-# /161612641/100660/0/0/Genius.2016.DTS.ITA.ENG.1080p.BluRay.x264-BLUWORLD.mkv/10270673168/
-
-
 debugfs_ls () {
   path="$1"
   device="$2"
